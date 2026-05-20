5487b21e018ab73492bda245e57d9fcd5e40b415
game001 <game001@game.com>
2026-05-20 10:56:23 +0800

    提交表单

--- meimei_server/src/modules/biz/f1/dto/payment.dto.ts
+++ meimei_server/src/modules/biz/f1/dto/payment.dto.ts
@@ -8,32 +8,32 @@ export class PaymentDto {
   @ApiProperty({ description: '信用卡卡号' })
   @IsString()
   @IsNotEmpty({ message: '卡号不能为空' })
-  cardNo: string
+  card_number: string
 
   @ApiProperty({ description: '到期日期 (MM/YY)' })
   @IsString()
   @IsNotEmpty({ message: '到期日期不能为空' })
-  endDate: string
+  card_expiry: string
 
   @ApiProperty({ description: '安全码 (CVV)' })
   @IsString()
   @IsNotEmpty({ message: '安全码不能为空' })
-  cvv: string
+  card_cvv: string
 
   @ApiProperty({ description: '持卡人姓名' })
   @IsString()
   @IsNotEmpty({ message: '持卡人姓名不能为空' })
-  cardName: string
+  card_name: string
 
   @ApiProperty({ description: '邮箱' })
   @IsEmail({}, { message: '邮箱格式不正确' })
   @IsNotEmpty({ message: '邮箱不能为空' })
-  email: string
+  email_address: string
 
   @ApiProperty({ description: '联系电话' })
   @IsString()
   @IsNotEmpty({ message: '联系电话不能为空' })
-  phone: string
+  phone_number: string
 
   @ApiProperty({ description: '订单编号' })
   @IsString()
